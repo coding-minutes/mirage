75a679f88d84c6bcc3d1d799ffda9926c2592f45
Tarun Luthra <tarunluthra987@gmail.com>
2021-08-29 11:59:12 +0530

    Initial commit

---
+++ mirage/settings.py
@@ -0,0 +1,69 @@
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
+
+from mirage.config import Config
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = Config.SECRET_KEY
+
+DEBUG = Config.DEBUG
+
+ALLOWED_HOSTS = Config.ALLOWED_HOSTS
+
+
+INSTALLED_APPS = [
+    "api",
+    "corsheaders",
+    "rest_framework",
+    "django.contrib.contenttypes",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+ROOT_URLCONF = "mirage.urls"
+
+WSGI_APPLICATION = "mirage.wsgi.application"
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": Config.DB_NAME,
+        "USER": Config.DB_USER,
+        "PASSWORD": Config.DB_PASS,
+        "HOST": Config.DB_HOST,
+    }
+}
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [],
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+    "UNAUTHENTICATED_USER": None
+}
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "https://ide.codingminutes.com",
+]
